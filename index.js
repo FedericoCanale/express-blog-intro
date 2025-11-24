bae2558a53851b050873227d52ab2f7fbf2dbae8
FedericoCanale <icocanale@gmail.com>
2025-11-24 12:17:24 +0100

    add array of objects to index.js with img files

--- index.js
+++ index.js
@@ -3,8 +3,51 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
+
+const posts = [
+    {
+        id: 1,
+        title: "Ciambellone",
+        content: "1",
+        image: "/img/ciambellone.jpeg",
+        tags: ["dolce", "colazione"]
+    },
+    {
+        id: 2,
+        title: "Cracker alla barbabietola",
+        content: "2",
+        image: "/img/cracker_barbabietola.jpeg",
+        tags: ["salato", "snack", "barbabietola"]
+    },
+    {
+        id: 3,
+        title: "Pane fritto dolce",
+        content: "3",
+        image: "/img/pane_fritto_dolce.jpeg",
+        tags: ["dolce", "tradizione"]
+    },
+    {
+        id: 4,
+        title: "Pasta alla barbabietola",
+        content: "4",
+        image: "/img/pasta_barbabietola.jpeg",
+        tags: ["pasta", "primo", "barbabietola"]
+    },
+    {
+        id: 5,
+        title: "Torta paesana",
+        content: "5",
+        image: "/img/torta_paesana.jpeg",
+        tags: ["dolce", "pane", "lombardia"]
+    }
+];
+
 app.get("/", (req, res) => {
     res.send("Server del mio blog");
+});
+
+app.get("/bacheca", (req, res) => {
+    res.json(posts);
 });
 
 app.listen(PORT, () => {
