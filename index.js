7bc776a456b72bc8fa0c8d1cc781dfce182e9ec9
FedericoCanale <icocanale@gmail.com>
2025-11-24 12:02:30 +0100

    add basic express server

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const app = express();
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+    res.send("Server del mio blog");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server in ascolto su http://localhost:${PORT}`);
+});
